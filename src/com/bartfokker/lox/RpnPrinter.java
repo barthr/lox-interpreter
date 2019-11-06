e0ae5e7f60537c03f765d80a62837a753a640c00
barthr <bartfokker@gmail.com>
2019-11-06 11:09:29 +0100

    enhance example for rpn printer

--- src/com/bartfokker/lox/RpnPrinter.java
+++ src/com/bartfokker/lox/RpnPrinter.java
@@ -44,20 +44,13 @@ public class RpnPrinter implements Expr.Visitor<String> {
     }
 
     public static void main(String[] args) {
-
         Expr expression = new Expr.Binary(
-                new Expr.Binary(
-                        new Expr.Literal(1),
-                        new Token(TokenType.MINUS, "+", null, 1),
-                        new Expr.Literal(2)
-                ),
-                new Token(TokenType.PLUS, "+", null, 1),
-                new Expr.Binary(
-                        new Expr.Literal(10),
-                        new Token(TokenType.PLUS, "+", null, 1),
-                        new Expr.Literal(3)
-                )
-        );
+                new Expr.Unary(
+                        new Token(TokenType.MINUS, "-", null, 1),
+                        new Expr.Literal(123)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Literal(45.67)));
 
 
         System.out.println(new RpnPrinter().print(expression));
